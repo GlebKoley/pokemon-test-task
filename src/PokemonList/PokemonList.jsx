96c3034bb19527cad0469922f89f2e03a91f0d7f
Gleb Koley <gleb.koley@gmail.com>
2023-08-27 18:25:29 +0300

    delete trash in components

--- src/PokemonList/PokemonList.jsx
+++ src/PokemonList/PokemonList.jsx
@@ -24,7 +24,7 @@ const PokemonList = ({ setCurrentPokemon }) => {
          </div>
       );
    }
-   console.log("render");
+
    return (
       <div className="pokemon_list">
          {pokemonData.length === 0 ? (
